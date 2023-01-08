73d213e279850cd150e3198df2e84698af1c0573
prisar <prisar230@gmail.com>
2023-01-08 19:15:56 +0530

    tb

--- app/bhumi/app/src/main/java/com/kulik/bhumi/MainActivity.kt
+++ app/bhumi/app/src/main/java/com/kulik/bhumi/MainActivity.kt
@@ -3,17 +3,17 @@ package com.kulik.bhumi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kulik.bhumi.ui.theme.BhumiTheme
+import com.kulik.bhumi.ui.theme.Black
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,23 +43,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float
+) {
+    Text(
+        text = text,
+        Modifier
+            .border(1.dp, Black)
+            .weight(weight)
+            .padding(8.dp)
+    )
+}
+
+@Composable
+fun TableScreen() {
+    // Just a fake data... a Pair of Int and String
+    val tableData = (1..100).mapIndexed { index, item ->
+        index to "Item $index"
+    }
+    // Each cell of a column must have the same weight.
+    val column1Weight = .3f // 30%
+    val column2Weight = .7f // 70%
+    // The LazyColumn will be our table. Notice the use of the weights below
+    LazyColumn(Modifier.fillMaxSize().padding(16.dp)) {
+        // Here is the header
+        item {
+            Row(Modifier.background(Color.Gray)) {
+                TableCell(text = "Column 1", weight = column1Weight)
+                TableCell(text = "Column 2", weight = column2Weight)
+            }
+        }
+        // Here are all the lines of your table.
+        items(tableData) {
+            val (id, text) = it
+            Row(Modifier.fillMaxWidth()) {
+                TableCell(text = id.toString(), weight = column1Weight)
+                TableCell(text = text, weight = column2Weight)
+            }
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
 //    Text(text = "Hello $name!")
     val shape = CircleShape
-
+    var enabled = true
     Column (verticalArrangement= Arrangement.SpaceBetween) {
         Text(
             text = "Land maps",
             style = TextStyle( color = White,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center),
-                    modifier = Modifier.fillMaxWidth()
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center),
+            modifier = Modifier.fillMaxWidth()
                         .padding(16.dp)
                         .border(2.dp, MaterialTheme.colors.secondary, shape)
                         .background(MaterialTheme.colors.primary, shape)
                         .padding(16.dp)
+                .clickable(enabled = enabled) {
+                    enabled = false
+//                    onClick()
+                },
         )
 
+        TableScreen()
+        
         Column(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
