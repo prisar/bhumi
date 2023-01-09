20481679dd2e95e298f6e018f43d96d361c93aa4
prisar <prisar230@gmail.com>
2023-01-09 12:03:38 +0530

    zoom

--- app/bhumi/app/src/main/java/com/kulik/bhumi/LandActivity.kt
+++ app/bhumi/app/src/main/java/com/kulik/bhumi/LandActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,8 +15,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,11 +48,33 @@ class LandActivity : ComponentActivity() {
 
 @Composable
 fun MapImage() {
-    Image(
-        painter = painterResource(id = R.drawable.municipality_map),
-        contentDescription = "Raiganj Map",
-        modifier = Modifier.fillMaxWidth()
-    )
+    val scale = remember { mutableStateOf(1f) }
+    val rotationState = remember { mutableStateOf(1f) }
+    Box(
+        modifier = Modifier
+            .clip(RectangleShape) // Clip the box content
+            .fillMaxSize() // Give the size you want...
+            .background(Color.Gray)
+            .pointerInput(Unit) {
+                detectTransformGestures { centroid, pan, zoom, rotation ->
+                    scale.value *= zoom
+                    rotationState.value += rotation
+                }
+            }
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.municipality_map),
+            contentDescription = "Raiganj Map",
+            modifier = Modifier.fillMaxWidth()
+                .align(Alignment.Center) // keep the image centralized into the Box
+                .graphicsLayer(
+                    // adding some zoom limits (min 50%, max 200%)
+                    scaleX = maxOf(.5f, minOf(3f, scale.value)),
+                    scaleY = maxOf(.5f, minOf(3f, scale.value)),
+                    rotationZ = rotationState.value
+                )
+        )
+    }
 }
 
 @Composable
